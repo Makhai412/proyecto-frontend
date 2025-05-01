ba5299c041c7fd800afa0280c22009c2ed1ec20a
FRANGONICOLAS <146036815+FRANGONICOLAS@users.noreply.github.com>
2025-04-30 23:15:42 -0500

    PAGINA DE HOME v1

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import './App.css'
 //import {Login} from './pages/Login'
-import { Register } from './pages/Register'
+//import { Register } from './pages/Register'
 //import { Auditory } from './pages/Auditory'
+import { Home } from './pages/Home'
 
 function App() {
 
 
   return (
     <>
-      <Register />
+      <Home />
     </>
   )
 }
